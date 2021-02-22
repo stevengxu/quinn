dfdb05847d08eca8c524d9a87403970bea1a5126
Steven Xu <stevenxu1995@gmail.com>
2021-02-22 11:07:12 -0500

    add comments for example codes

--- R/example_1d.R
+++ R/example_1d.R
@@ -2,47 +2,64 @@ library(tidyverse)
 library(ggplot2)
 library(latex2exp)
 source("quinn.R")
+#Read in data
 data <- read.csv("../data/hurricane.csv")
 X <- data$Year
 y <- data$WmaxST
 
+#min-max normalize covariates and response
 min_X <- min(X)
 max_X <- max(X)
-min_y <- min(y)-0.001
+min_y <- min(y)-0.001 #small shifts from the boundary, can be arbitrarly small
 max_y <- max(y)+0.001
 
 X <- (X-min_X)/(max_X-min_X)
 z <- (y-min_y)/(max_y-min_y)
 
+#Create model input for training by specifying number of hidden neurons, 
+#number of spline knots, and dimension of the covariate vector
 train.model <- list(n.hidden=5,n.knots=9,n.var=1)
+
+#Specifying number of iterations and warmups, optionally can specify thinning
 iter=2000
 warmup=500
 
+#Run MCMC
 mcmc <- quinn_samp(X=X,z=z,train.model=train.model,iter=iter,warmup=warmup)
 
+#Extract posterior samples, here they are the last 1000 iterations
 post_id <- seq(iter-999,iter)
 post <- mcmc$par[post_id,]
 
+#Create model input for fitting/prediction
+#n.z is the grid size for interpolating the CDF and QF
 pred.model <- c(train.model,list(n.z=101,samp=post))
 
+#Quantile levels at which predictions are sought
 tau <- seq(0.05,0.95,0.05)
 
+#X values at which predictions are sought
 X.grid <- seq(0,1,length.out = 101)
 
+#Predict the QFs
 q.pred <- quinn_pred(pred.model=pred.model,newX=X.grid,tau=tau)
 
+#Back-transform to original scale
 q.pred <- q.pred*(max_y-min_y)+min_y
 
+#Post-processing for plotting
 qplot.df = as.data.frame(cbind(X.grid*(max_X-min_X)+min_X,q.pred))
 
 colnames(qplot.df) = c("X", paste("tau",1:length(tau),sep="."))
 
 qplot.df = gather(qplot.df,tau,y,tau.1:tau.19,factor_key=TRUE)
 
+
+#Plot the data overlaid with simultaneous estimate of quantile curves
 ggplot()+geom_point(data = data, aes(Year,WmaxST))+labs(x = "Year", y="WmaxST")+
   geom_line(data = qplot.df, aes(X,y,col = tau))+scale_color_discrete(name = expression(tau), labels = seq(0.05,0.95,0.05))
 
-
+#Obtain posterior samples of QF for a specific covariate value
 qf.post <- matrix(nrow=length(post_id),ncol=length(tau))
 for(i in 1:length(post_id))
 {
@@ -54,6 +71,7 @@ qf.post.df <- as.data.frame(cbind(rep(tau,length(post_id)),c(t(qf.post))))
 qf.post.df$V3 <- rep(paste0("id.",1:length(post_id)),each=length(tau))
 colnames(qf.post.df) <- c("tau","qf","id")
 
+#Plot the posterior samples of QF for a specific covariate value
 ggplot()+geom_line(data = qf.post.df, aes(tau,qf,group=id,color=id),alpha=0.05)+theme_bw()+
   labs(x=TeX("$\\tau$"),y="",title = TeX("Posterior distribution of $Q(\\tau |Year=2001),\\tau\\in\\[0.05,0.95\\]$"))+
   theme(text = element_text(size = 18),axis.text.y = element_text(size = 18),axis.text.x = element_text(size = 18))+guides(color=FALSE)
--- R/example_2d.R
+++ R/example_2d.R
@@ -3,6 +3,7 @@ library(plotly)
 source("quinn.R")
 source("ALEplot_qr.R")
 
+#Simulate a bivariate data set
 set.seed(1)
 n <- 500
 X <- matrix(runif(n*2),ncol=2)
@@ -11,33 +12,46 @@ gx <- sqrt(2*(X[,1]^2+X[,2]^2))
 eps <- rnorm(n,0,1)
 y <- fx + gx*eps
 
-min_y <- min(y)-0.001
+#min-max normalize covariates and response
+min_y <- min(y)-0.001 #small shifts from the boundary, can be arbitrarly small
 max_y <- max(y)+0.001
 
 z <- (y-min_y)/(max_y-min_y)
 
-
+#Create model input for training by specifying number of hidden neurons, 
+#number of spline knots, and dimension of the covariate vector
 train.model <- list(n.hidden=8,n.knots=11,n.var=2)
+
+#Specifying number of iterations and warmups, optionally can specify thinning
 iter=2000
 warmup=500
 
+#Run MCMC
 mcmc <- quinn_samp(X=X,z=z,train.model=train.model,iter=iter,warmup=warmup)
 
+#Extract posterior samples, here they are the last 1000 iterations
 post_id <- seq(iter-999,iter)
 post <- mcmc$par[post_id,]
 
+#Create model input for fitting/prediction
+#n.z is the grid size for interpolating the CDF and QF
 pred.model <- c(train.model,list(n.z=101,samp=post))
 
+#Quantile levels at which predictions are sought
 tau <- seq(0.05,0.95,0.05)
 
+#X values at which predictions are sought
 X1.grid <- seq(0,1,length.out = 51)
 X2.grid <- seq(0,1,length.out = 51)
 X.grid <- as.matrix(expand.grid(X1.grid,X2.grid))
 
+#Predict the QFs
 q.pred <- quinn_pred(pred.model=pred.model,newX=X.grid,tau=tau)
 
+#Back-transform to original scale
 q.pred <- q.pred*(max_y-min_y)+min_y
 
+#Plot quantile surfaces, here tau = 0.05, 0.5, 0.95
 Q1 <- q.pred[1,]
 Q2 <- q.pred[10,]
 Q3 <- q.pred[19,]
@@ -50,10 +64,20 @@ fig <- fig %>% add_surface(x = X2.grid, y = X1.grid, z = Q1) %>%
 
 fig
 
+#Wrapper prediction function to be feed into ALEPlot_qr
 pred.fun <- function(pred.model,newX,tau){
   q.pred <- quinn_pred(pred.model=pred.model,newX=newX,tau=tau)
   q.pred <- q.pred*(max_y-min_y)+min_y
   return(q.pred)
 }
 
+#Estimate marginal quantile main effect of X_2 for tau = 0.05,...,0.95
 q.ale_V2 <- ALEPlot_qr(X=X,pred.model=pred.model,pred.fun=pred.fun,tau=tau,J=2,K=50)
+
+#Plot the quantile ALE estimate against X_2
+plot(NULL,NULL,xlab="",ylab="",xlim=range(q.ale_V2[[1]]),ylim=range(q.ale_V2[[2]][,10]),axes=F)
+axis(side = 1, at = round(seq(min(q.ale_V2[[1]]),max(q.ale_V2[[1]][,10]), length.out = 5)),cex.axis = 1.5)
+axis(side = 2, at = round(seq(min(q.ale_V2[[2]][,10]),max(q.ale_V2[[2]][,10]), length.out = 5)),cex.axis = 1.5)
+title(xlab=expression(italic(X)),line=2.5,cex.lab = 1.5)
+title(ylab=latex2exp::TeX("$\\bar{Q}(\\tau,x)$"),line=2,cex.lab = 1.7)
+grid (NULL,NULL, lty = 3)
